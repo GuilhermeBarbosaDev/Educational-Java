5743501aa9a8117ffd9401dcc2474257322c2277
GuilhermeBarbosaDev <guilhermebarbosajob@gmail.com>
2022-07-13 09:43:32 -0300

    little "game"

---
+++ src/switchCheck/switchFunnyReport.java
@@ -0,0 +1,19 @@
+package switchCheck;
+
+import java.util.Scanner;
+
+public class switchFunnyReport {
+
+
+    int options;
+
+    Scanner read = new Scanner(System.in);
+    public switchFunnyReport(){
+
+        System.out.println("Your time to choose!");
+    }
+
+    public static void main(String[] args) {
+    new switchFunnyReport();
+    }
+}

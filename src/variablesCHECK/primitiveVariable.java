609bc8585c49add8a0a63c92a0dc7abd053ce299
GuilhermeBarbosaDev <guilhermebarbosajob@gmail.com>
2022-07-12 22:06:25 -0300

    basic variables

---
+++ src/variablesCHECK/primitiveVariable.java
@@ -0,0 +1,16 @@
+package variablesCHECK;
+
+public class primitiveVariable {
+
+    public static void main(String[] args) {
+
+        String name = "Guilherme";
+        String address = "Av Brigadeiro";
+        Double salary = 2500.000;
+        String report = "My name is " + name + ", and i lived in " + address +
+                " confirming that i received the salary of " + salary;
+
+        System.out.println(report);
+    }
+
+}

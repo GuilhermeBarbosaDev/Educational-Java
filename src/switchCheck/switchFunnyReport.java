e840f443f8d468ef2b96bef2788e26ca862a5e13
GuilhermeBarbosaDev <guilhermebarbosajob@gmail.com>
2022-07-13 13:27:58 -0300

    implementing on switchFunnyReport

--- src/switchCheck/switchFunnyReport.java
+++ src/switchCheck/switchFunnyReport.java
@@ -11,6 +11,16 @@ public class switchFunnyReport {
     public switchFunnyReport(){
 
         System.out.println("Your time to choose!");
+        System.out.println("1-live forever\n2-live one month with your beloved");
+        options = read.nextInt();
+        switch (options){
+            case 1:
+                System.out.println("depression for eternity");
+                    break;
+            case 2:
+                System.out.println("one month of limited happiness");
+                    break;
+        }
     }
 
     public static void main(String[] args) {
